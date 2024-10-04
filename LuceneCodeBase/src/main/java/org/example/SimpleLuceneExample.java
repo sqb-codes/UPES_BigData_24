6330624827e622d6f89d715aeaa667b2c8779654
sqb-codes <150049400+sqb-codes@users.noreply.github.com>
2024-10-04 20:03:22 +0530

    Add files via upload

--- LuceneCodeBase/src/main/java/org/example/SimpleLuceneExample.java
+++ LuceneCodeBase/src/main/java/org/example/SimpleLuceneExample.java
@@ -4,8 +4,15 @@ import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.ByteBuffersDirectory;
 import org.apache.lucene.store.Directory;
 
@@ -20,7 +27,7 @@ public class SimpleLuceneExample {
         writer.addDocument(doc);
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException {
         // 1. Create a StandardAnalyzer Object
         // tokenize the text into words and remove stopwords
         StandardAnalyzer analyzer = new StandardAnalyzer();
@@ -40,6 +47,39 @@ public class SimpleLuceneExample {
          * 4. Create IndexWriter - Adds the document to the index
          */
         IndexWriter writer = new IndexWriter(index, config);
+
+        addDocument(writer, "Java Programming", "Java is best object oriented programming");
+        addDocument(writer, "Java Concepts", "Java is a powerful programming");
+        addDocument(writer, "Java OOPS", "OOPS are must have in any programming");
+        addDocument(writer, "Java Detail", "Java offers best detailing for OOPS");
+        writer.close();
+
+        /*
+         *  Searching
+         *  QueryParser - Parses the search query, target the content field
+         */
+        String queryString = "Java";
+        QueryParser parser = new QueryParser("content", analyzer);
+        Query query = parser.parse(queryString);
+
+        /*
+         * IndexSearcher - Performs the search on the index
+         * DirectoryReader - Reads the indexed documents for searching
+         */
+        DirectoryReader reader = DirectoryReader.open(index);
+        IndexSearcher searcher = new IndexSearcher(reader);
+
+        // TopDocs - Holds the search results
+        TopDocs results = searcher.search(query, 2);
+
+        System.out.println("Total Hits : " + results.totalHits.value);
+        for(ScoreDoc score: results.scoreDocs) {
+            // Relevance Scoring - for each document is computed based on Lucene's scoring algorithm
+            // Example - TF-IDF (Term Frequency - Inverse Document Frequency)
+            Document doc = searcher.doc(score.doc);
+            System.out.println("Title: " + doc.get("title"));
+        }
+
 
     }
 }
